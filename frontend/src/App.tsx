edcb28bad09aa54887bdd4a4fdffd2710a6f9794
Mohamedfathi540 <mohamedfathe540@gmail.com>
2026-02-24 09:20:47 -0500

    remove settings page route from App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { MainLayout } from "./components/layout/MainLayout";
 import { ChatPage } from "./pages/ChatPage";
 import { LibraryDocsPage } from "./pages/LibraryDocsPage";
 import { IndexInfoPage } from "./pages/IndexInfoPage";
-import { SettingsPage } from "./pages/SettingsPage";
 import { SearchPage } from "./pages/SearchPage";
 
 const queryClient = new QueryClient({
@@ -26,7 +25,6 @@ function App() {
             <Route path="library-docs" element={<LibraryDocsPage />} />
             <Route path="search" element={<SearchPage />} />
             <Route path="index" element={<IndexInfoPage />} />
-            <Route path="settings" element={<SettingsPage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
